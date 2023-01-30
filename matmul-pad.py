84ef345f4455f3e0fd042232a5a9b7bc90e26e6e
Mark Saroufim <marksaroufim@gmail.com>
2023-01-30 23:00:20 +0000

    refactor

--- matmul-pad.py
+++ matmul-pad.py
@@ -1,17 +1,12 @@
 import torch
 import time
 import matplotlib.pyplot as plt
+import csv
+from tqdm import tqdm
+
 # Enable tensor cores
 torch.set_float32_matmul_precision('high')
-from tqdm import tqdm
-import csv
-
-f = open('output', 'w')
-writer = csv.writer(f)
-header = ["outer_size", "inner_size", "duration", "pad"]
-writer.writerow(header)
-cuda = torch.device('cuda') 
-
+cuda = torch.device('cuda')
 
 def largest_closest_multiple(n, k=16):
     if n % k == 0:
@@ -19,47 +14,59 @@ def largest_closest_multiple(n, k=16):
     else:
         return n + k - (n % k)
 
-durations_no_pad = []
-durations_pad = []
+def matmul_test(size1, size2, pad=False):
+    if pad:
+        size1 = largest_closest_multiple(size1)
+        size2 = largest_closest_multiple(size2)
+    
+    tensor1 = torch.randn(size1,size2, device=cuda)
+    tensor2 = torch.randn(size2, size1, device=cuda)
 
-for size1 in tqdm(range(10000, 20000, 1000)):
-    for size2 in tqdm(range(10000, 20000, 1000)):
-        tensor1 = torch.randn(size1,size2, device=cuda)
-        tensor2 = torch.randn(size2, size1, device=cuda)
+    tensor1.to(torch.int8)
+    tensor2.to(torch.int8)
 
-        tensor1.to(torch.int8)
-        tensor2.to(torch.int8)
+    tic = time.time()
+    result = torch.matmul(tensor1, tensor2)
+    torch.cuda.synchronize()
+    toc = time.time()
+    return toc - tic
 
-        tic = time.time()
-        result = torch.matmul(tensor1, tensor2)
-        torch.cuda.synchronize()
+def write_to_csv(file, data):
+    writer = csv.writer(file)
+    header = ["outer_size", "inner_size", "duration", "pad"]
+    writer.writerow(header)
+    for row in data:
+        writer.writerow(row)
 
-        toc = time.time()
-        durations_no_pad.append(toc - tic)
+def run_tests(sizes, pad=False):
+    durations = []
+    data = []
+    for size1 in sizes:
+        for size2 in sizes:
+            duration = matmul_test(size1, size2, pad)
+            durations.append(duration)
+            data.append([str(size1), str(size2), str(duration), str(pad)])
+    return durations, data
 
-        writer.writerow([str(size1), str(size2), str(toc - tic), "false"])
+def plot_results(durations_no_pad, durations_pad):
+    plt.plot(durations_no_pad[1:], label="no-pad")
+    plt.plot(durations_pad[1:], label="pad-to-16")
+    plt.ylabel('Time taken in seconds')
+    plt.xlabel('Dimension of Tensor size1, size2')
+    plt.legend()
 
-        tensor1 = torch.randn(largest_closest_multiple(size1),largest_closest_multiple(size2), device=cuda)
-        tensor2 = torch.randn(largest_closest_multiple(size2), largest_closest_multiple(size1), device=cuda)
+    plt.show()
+    plt.savefig('matmul-pad-nopad-big.png')
 
-        tensor1.to(torch.int8)
-        tensor2.to(torch.int8)
-        tic = time.time()
-        result = torch.matmul(tensor1, tensor2)
-        torch.cuda.synchronize()
-        toc = time.time()
-        durations_pad.append(toc - tic)
+def main():
+    sizes = range(10000, 20000, 1000)
+    durations_no_pad, data_no_pad = run_tests(sizes, False)
+    durations_pad, data_pad = run_tests(sizes, True)
 
-        writer.writerow([str(largest_closest_multiple(size1)), str(largest_closest_multiple(size2)), str(toc - tic), "true"])
+    with open('output', 'w') as f:
+        write_to_csv(f, data_no_pad + data_pad)
 
+    plot_results(durations_no_pad, durations_pad)
 
-
-plt.plot(durations_no_pad[1:], label="no-pad")
-plt.plot(durations_pad[1:], label="pad-to-16")
-plt.ylabel('Time taken in seconds')
-plt.xlabel('Dimension of Tensor size1, size2')
-plt.legend()
-
-plt.show()
-plt.savefig('matmul-pad-nopad-big.png')
-f.close()
+if __name__ == "__main__":
+    main()
